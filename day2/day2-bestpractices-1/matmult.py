31c909c7ec99e458c330adb2cd80c3f0aa03bc32
boehmax <maximilian.bohm@kemi.uu.se>
2024-02-21 00:21:45 +0100

    implementent the numpy package in the code.. fells illegal but its much faste that way

--- day2/day2-bestpractices-1/matmult.py
+++ day2/day2-bestpractices-1/matmult.py
@@ -1,30 +1,15 @@
-# Program to multiply two matrices using nested loops
+import numpy as np
 import random
 
 N = 250
 
 # NxN matrix
-X = []
-for i in range(N):
-    X.append([random.randint(0,100) for r in range(N)])
+X = np.random.randint(0, 100, size=(N, N))
 
 # Nx(N+1) matrix
-Y = []
-for i in range(N):
-    Y.append([random.randint(0,100) for r in range(N+1)])
+Y = np.random.randint(0, 100, size=(N, N+1))
 
-# result is Nx(N+1)
-result = []
-for i in range(N):
-    result.append([0] * (N+1))
+# Multiply matrices
+result = np.dot(X, Y)
 
-# iterate through rows of X
-for i in range(len(X)):
-    # iterate through columns of Y
-    for j in range(len(Y[0])):
-        # iterate through rows of Y
-        for k in range(len(Y)):
-            result[i][j] += X[i][k] * Y[k][j]
-
-for r in result:
-    print(r)
+print(result)
