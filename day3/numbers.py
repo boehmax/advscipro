b93b8532ef91819a64ff5d7d7a9c7b87dac9fd38
boehmax <maximilian.bohm@kemi.uu.se>
2024-02-22 08:46:17 +0100

    Finished the NumPy excercise :) my brain is not equipped for this...

--- day3/numbers.py
+++ day3/numbers.py
@@ -34,3 +34,28 @@ h
 
 i = np.array([[0,1],[1,0]])
 np.tile(i,(4,4))
+
+j = np.arange(11)
+j[4:8] = j[4:8]*-1
+print(j)
+
+k = np.random.random(10)
+np.sort(k)
+
+l_A = np.random.randint(0,2,5)
+l_B = np.random.randint(0,2,5)
+equal = l_A == l_B
+print(equal)
+
+
+m = np.arange(10, dtype=np.int32)
+print(m.dtype)
+m = np.square(m)
+print(m.dytpe)
+
+n_A = np.arange(9).reshape(3,3)
+n_B = n_A + 1
+n_C = np.dot(n_A,n_B)
+n_D = np.diag(n_C)
+print(n_D)
+
